709190a5aa34c7cab5231636de3aef48d3cd60e9
tandatng <ngtandat.dl@gmail.com>
2018-07-21 11:28:30 +0700

    [UI] Modify ui

--- html/js/index.js
+++ html/js/index.js
@@ -6,7 +6,7 @@ function menu() {
 
       if (document.querySelectorAll('.ka-menu')[0].childNodes[i]) {
         document.querySelectorAll('.ka-menu')[0].childNodes[i].addEventListener("click", function () {
-          if(this.className.match(mActive)){
+          if (this.className.match(mActive)) {
             this.classList.remove('ka-active');
             this.parentElement.parentElement.classList.remove("ka-active");
             return;
@@ -34,3 +34,40 @@ function menu() {
 }
 
 window.addEventListener("DOMContentLoaded", menu);
+
+// SCROLL =================================================
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function () {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
